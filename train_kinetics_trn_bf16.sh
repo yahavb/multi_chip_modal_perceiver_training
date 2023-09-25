f1ecc252a328ae88edddc4dd4f9ef6da8adc1908
Ubuntu <ubuntu@ip-172-31-13-77.us-west-2.compute.internal>
2023-09-25 19:25:05 +0000

    trn

--- train_kinetics_trn_bf16.sh
+++ train_kinetics_trn_bf16.sh
@@ -1,5 +1,5 @@
 export MALLOC_ARENA_MAX=32
-#export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=1
 XLA_USE_BF16=1 NEURON_CC_FLAGS='--retry_failed_compilation --internal-max-instruction-limit=10000000 --enable-experimental-O1 --internal-build-with-users --enable-saturate-infinity' torchrun --nproc_per_node=32 train_mp_trn.py \
   --config_file_path config/main.yaml \
   --do_eval \
